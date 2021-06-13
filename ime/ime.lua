181dd52a50532e834291671893f58ced5afeb9df
Jed <jed@jeddd.com>
2021-06-13 19:26:04 +0800

    update ime

--- ime/ime.lua
+++ ime/ime.lua
@@ -1,9 +1,26 @@
-local last_ime = 'Chinese'
+require("hs.timer")
 
-local lang2sourceid = {}
-lang2sourceid['English'] = 'com.apple.keylayout.ABC'
-lang2sourceid['Chinese'] = 'com.apple.inputmethod.SCIM.ITABC'
--- lang2sourceid['Chinese'] = 'im.rime.inputmethod.Squirrel.Rime'
+local last_ime = 'Chinese'  -- default IME
+
+local lang2sourceid = {
+    ['English'] = 'com.apple.keylayout.ABC',
+    ['Chinese'] = 'com.apple.inputmethod.SCIM.ITABC',
+    -- ['Chinese'] = 'im.rime.inputmethod.Squirrel.Rime'
+}
+
+local preset = {
+    ['Spotlight'] = 'English',
+    ['Code'] = 'English',
+    ['Sublime Text'] = 'English',
+    ['iTerm2'] = 'English',
+    ['Terminal'] = 'English',
+    ['Finder'] = 'Chinese',
+    ['WeChat'] = 'Chinese',
+    ['QQ'] = 'Chinese',
+    ['Microsoft OneNote'] = 'Chinese',
+}
+
+local cache = {}
 
 local function setIme(ime)
     local current_ime = hs.keycodes.currentSourceID()
@@ -16,23 +33,9 @@ local function setIme(ime)
     if ime == 'English' then
         hs.keycodes.currentSourceID(lang2sourceid['English'])
     elseif ime == 'Chinese' then
-        -- hs.keycodes.currentSourceID('im.rime.inputmethod.Squirrel.Rime')
         hs.keycodes.currentSourceID(lang2sourceid['Chinese'])
     end
 end
-
-local preset = {}
-preset['Spotlight'] = 'English'
-preset['Code'] = 'English'
-preset['Sublime Text'] = 'English'
-preset['iTerm2'] = 'English'
-preset['Terminal'] = 'English'
-preset['Finder'] = 'Chinese'
-preset['WeChat'] = 'Chinese'
-preset['QQ'] = 'Chinese'
-preset['Microsoft OneNote'] = 'Chinese'
-
-local cache = {}
 
 function doAppLaunched(appname)
     if preset[appname] ~= nil then
@@ -77,12 +80,11 @@ hs.hotkey.bind({'ctrl', 'cmd'}, ".", function()
 end)
 
 hs.window.filter.new('Spotlight')
-:subscribe(hs.window.filter.hasWindow, function()
-             doAppActivated('Spotlight')
-          end)
+    :subscribe(hs.window.filter.hasWindow, function()
+        doAppActivated('Spotlight')
+    end)
 
 hs.application.watcher.new(function (appname, eventtype, appobj)
-    print(appname)
     if eventtype == hs.application.watcher.launched then
         doAppLaunched(appname)
     elseif eventtype == hs.application.watcher.activated then
@@ -94,3 +96,9 @@ hs.application.watcher.new(function (appname, eventtype, appobj)
     end
 end):start()
 print('======= RESTARTING =======')
+
+-- hs.caffeinate.watcher.new(function (eventtype)
+--     if eventtype == hs.caffeinate.watcher.systemDidWake
+--         hs.reload()
+--     end
+-- end):start()
