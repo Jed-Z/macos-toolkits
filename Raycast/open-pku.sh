07f45c252e2a55a3989fe6616dd8e8ad34b374b8
Jed <jed@jeddd.com>
2021-11-02 15:13:31 +0800

    raycast: add open folder

---
+++ Raycast/open-pku.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open PKU Courses folder
+# @raycast.mode silent
+# @raycast.packageName Navigation
+
+# Optional parameters:
+# @raycast.icon images/folder-desktop.png
+
+# Documentation:
+# @raycast.description Opens the Desktop folder in the Finder.
+
+open ~/Desktop/PKU\ Courses
